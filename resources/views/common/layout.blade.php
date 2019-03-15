306003c5bded5a54bed98590a979867bf288e390
waonngionn <air_mito.0622@icloud.com>
2019-03-15 18:46:41 +0900

    説明文修正

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>【沼津高専限定】単位計算webアプリ</title>
+    <title>【沼津高専限定】卒業までに必要な単位計算webアプリ</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     <meta name="twitter:card" content="summary" />
-    <meta property="og:title" content="【沼津高専生限定】単位計算webアプリ" />
+    <meta property="og:title" content="【沼津高専生限定】卒業までに必要な単位計算webアプリ" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="http://waonn.sakura.ne.jp/calunitfornit/" />
     <meta property="og:image" content="http://waonn.sakura.ne.jp/calunitfornit/storage/calunitfornitmark.png" />
-    <meta property="og:site_name" content="沼津高専生限定単位計算webアプリ" />
+    <meta property="og:site_name" content="【沼津高専生限定】卒業までに必要な単位計算webアプリ" />
     <meta property="og:description" content="沼津高専生が卒業までに必要な単位数を計算します" />
 
 </head>
