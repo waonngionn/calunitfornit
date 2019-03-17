9b2223083cb36afdb87c19e8a6b6fb9fcf5817cd
waonngionn <air_mito.0622@icloud.com>
2019-03-17 17:59:10 +0900

    googleアドセンス

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -10,6 +10,14 @@
 
     gtag('config', 'UA-136334454-1');
     </script>
+    <!-- googleアドセンス -->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-1525658609073947",
+        enable_page_level_ads: true
+    });
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>【沼津高専限定】卒業までに必要な単位計算webアプリ</title>
